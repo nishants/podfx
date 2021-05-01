71f27a3d7eba303145181fb4e19c3e2d1292a32b
nishant <nishant.singh87@gmail.com>
2021-05-01 23:46:34 +0530

    Clean file names from ls.

--- docs/spikes/k8s-client/lib/ls/index.js
+++ docs/spikes/k8s-client/lib/ls/index.js
@@ -17,13 +17,32 @@ const getFiles = (exec, chosenNameSpace, podName, containerName, path = "/") => 
     output.push(utf8);
   });
 
+  const convertToFiles = (line) => {
+    const tokens = line.split(" ");
+    if(tokens.length < 7){
+      return null;
+    }
+    const size = tokens[4];
+    const time = `${tokens[5]} ${tokens[6]} ${tokens[7]}`;
+    const name = tokens.slice(8).join(" ");
+    if(name.endsWith("./")){
+      return null;
+    }
+    return tokens.slice(4).join(" ").trim();
+    return {
+      size,
+      time,
+      name
+    };
+  };
+
   const getFilesFrom = (output) => {
     const lines = output
       .map(o => o.split("\r\n"))
       .reduce((all, el) => [...all, ...el], [])
       .filter(a => a.length);
 
-    return lines;
+    return lines.map(convertToFiles).filter(f => !!f);
   };
 
   return new Promise(async (resolve, reject) => {
