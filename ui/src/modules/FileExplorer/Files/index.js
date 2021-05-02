5d1f69dd725fd673d705379b19ab1178d8f4eaed
nishant <nishant.singh87@gmail.com>
2021-05-02 12:37:20 +0530

    Select file on one click and open on double.

--- ui/src/modules/FileExplorer/Files/index.js
+++ ui/src/modules/FileExplorer/Files/index.js
@@ -5,14 +5,20 @@ const Files = ({getFiles}) => {
 
   const [currentPath, setCurrentPath] = React.useState('/');
   const [filesOnCurrentPath, setFilesOnCurrentPath] = React.useState([]);
+  const [selectedFileName, setSelectedFileName] = React.useState(null);
 
   React.useEffect(() => {
+    setSelectedFileName(null);
     getFiles(currentPath).then((files) => {
       setFilesOnCurrentPath(files);
     });
   }, [currentPath]);
 
   const select = (file) => {
+    setSelectedFileName(file.name);
+  };
+
+  const open = (file) => {
     if(file.isDir){
       setCurrentPath(`${currentPath}${file.name}/`);
     }
@@ -26,12 +32,19 @@ const Files = ({getFiles}) => {
 
   const items = filesOnCurrentPath.map((file) => {
     const iconClassName = `file-type-icon ${file.isDir ? "dir" : ""}`;
+    const selectedClassName = selectedFileName === file.name ? "selected" : "";
     return (
-      <li key={file.name} onDoubleClick={() => select(file)}>
+      <li
+        key={file.name}
+        onDoubleClick={() => open(file)}
+        onClick={() => select(file)}
+        className={selectedClassName}>
+
         <span className={iconClassName}>
           {file.isDir ? (<i className="fas fa-folder"></i>) : (<i className="fas fa-file"></i>)}
         </span>
         <label className='file-name'>{file.name}</label>
+
       </li>
     );
   });
